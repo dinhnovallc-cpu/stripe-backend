55cbeced16b668712facd108c22f349bb0481333
dinhnovallc-cpu <dinhnovallc@gmail.com>
2026-04-17 17:56:22 -0600

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -42,16 +42,20 @@ module.exports = async function handler(req, res) {
     });
 
     const session = await stripe.checkout.sessions.create({
-      mode: 'payment',
-      line_items,
+  mode: 'payment',
+  line_items: lineItems,
 
-      shipping_address_collection: {
-        allowed_countries: ['US'],
-      },
+  phone_number_collection: {
+    enabled: true
+  },
 
-      success_url: 'https://dinhnova.com/pages/thank-you',
-      cancel_url: 'https://dinhnova.com/cart',
-    });
+  shipping_address_collection: {
+    allowed_countries: ['US']
+  },
+
+  success_url: `${origin}/pages/success?session_id={CHECKOUT_SESSION_ID}`,
+  cancel_url: `${origin}/cart`
+});
 
     return res.status(200).json({ url: session.url });
   } catch (error) {
